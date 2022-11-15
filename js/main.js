e2f17ebd118fd50f43505ee0ecda0b1cdd06a871
JennyLamego2022 <jennylamego@hotmail.com>
2022-11-15 12:15:15 -0300

    Criando funcionalidades em JavaScript

--- js/main.js
+++ js/main.js
@@ -1,1 +1,14 @@
-//alert('Olá Mundo!!!')
+let pegaResultado = document.querySelector("#resultado")
+let pegaNumero1= document.querySelector("#numero1")
+let pegaNumero2= document.querySelector("#numero2")
+let pegaNumero3= document.querySelector("#numero3")
+let pegaNumero4= document.querySelector("#numero4")
+let pegaNumero5= document.querySelector("#numero5")
+
+
+
+
+
+pegaNumero1.addEventListener('click', ()=>{
+    pegaResultado.innerHTML = 'A nota escolhida foi  ${pegaNumero1.value}'
+})
