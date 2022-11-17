8b4bfb8eb5b6bb420a9c62d67c5787bc6f609eab
JennyLamego2022 <jennylamego@hotmail.com>
2022-11-17 10:07:36 -0300

    Correção do texto de exibição da nota

--- js/main.js
+++ js/main.js
@@ -8,19 +8,19 @@ let pegaNumero5= document.querySelector("#numero5")
 
 
 pegaNumero1.addEventListener('click', ()=>{
-    pegaResultado.innerHTML = `a nota  ${pegaNumero1.value}`
+    pegaResultado.innerHTML = ` a nota  ${pegaNumero1.value}`
   })
 pegaNumero2.addEventListener('click', ()=>{
-  pegaResultado.innerHTML = `a nota  ${pegaNumero2.value}`
+  pegaResultado.innerHTML = ` a nota  ${pegaNumero2.value}`
 })
 pegaNumero3.addEventListener('click', ()=>{
-    pegaResultado.innerHTML = `a nota  ${pegaNumero3.value}`
+    pegaResultado.innerHTML = ` a nota  ${pegaNumero3.value}`
 })
 pegaNumero4.addEventListener('click', ()=>{
-  pegaResultado.innerHTML = `a nota  ${pegaNumero4.value}`
+  pegaResultado.innerHTML = ` a nota  ${pegaNumero4.value}`
 })
 pegaNumero5.addEventListener('click', ()=>{
- pegaResultado.innerHTML = `a nota ${pegaNumero5.value}`
+ pegaResultado.innerHTML = ` a nota ${pegaNumero5.value}`
 })
 
 
